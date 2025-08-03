c617eede1123c6a60b5bf13c5077e7be5ea74f44
Leonid Gershenzon <leomayak81@gmail.com>
2025-08-03 15:03:10 +0300

    minor MemorySystem refactor

--- MemorySystem/MemorySystem.py
+++ MemorySystem/MemorySystem.py
@@ -1,4 +1,6 @@
 import logging
+from typing import Any, List
+
 from MemorySystem.WritingPatternDetector import FailureDetectedError
 
 logger = logging.getLogger("infra_logger." + __name__)
@@ -6,19 +8,19 @@ logger = logging.getLogger("infra_logger." + __name__)
 
 class MemorySystem:
 
-    def __init__(self, transmitter, detector, pattern_descriptor) -> None:
+    def __init__(self, transmitter: Any, detector: Any, pattern_descriptor: List[int]) -> None:
         self.__transmitter = transmitter
         self.__detector = detector
         self.__pattern_descriptor = pattern_descriptor
 
     def run(self) -> None:
-        transmission_chanel = self.__transmitter.start_frame_transmission()
+        transmission_channel = self.__transmitter.start_frame_transmission()
 
         logger.info(f"System starts frame transmission")
         for memory_write_len in self.__pattern_descriptor:
             try:
                 for frame in range(memory_write_len):
-                    frame = next(transmission_chanel)
+                    frame = next(transmission_channel)
                     self.__detector.process_incoming_frame(frame)
                 self.__detector.notify_mw_tx_end()
             except StopIteration:
@@ -29,5 +31,5 @@ class MemorySystem:
                 break
 
             logger.info(f'finished transferring: {memory_write_len} frames')
-        logger.info(f"Finished processing writing pattern")
+        logger.info(f"Writing pattern processing complete, in case of failure, check log")
         
